dc34cd05da67cd5d89d2b6e9f8bd63b190e5cde4
maxon-by <maxon.pro2015@gmail.com>
2020-04-17 18:44:30 +0300

    update podemonList

--- src/components/PokemonItem/PokemonItem.js
+++ src/components/PokemonItem/PokemonItem.js
@@ -3,8 +3,17 @@ import './PokeminItem.css'
 
 const PokemonItem = () => {
     return (
-        <div>
-            PokemonItem
+        <div className="pokemon-item">
+            <div className="pokemon-img">
+                <a href="#">
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" alt="#"/>
+                </a>
+            </div>
+            <div className="abilities">
+                <span>Poison</span>
+                <span>Grass</span>
+            </div>
+            <h4>Pokemon name</h4>
         </div>
     );
 };
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './PokemonList.css'
 import PokemonItem from "../PokemonItem";
 
 const PokemonList = () => {
     return (
         <div className="pokemon-list">
+            <PokemonItem/>
+            <PokemonItem/>
+            <PokemonItem/>
+            <PokemonItem/>
             <PokemonItem/>
             <PokemonItem/>
             <PokemonItem/>
